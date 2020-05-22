78dcd67a9d06bf5404c66b41312e9d073ff2d04d
ubuntu <you@example.com>
2020-05-22 16:08:26 -0700

    first commit

---
+++ 1-mem/.mem.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu/linux/tools/labs/skels/./kernel_api/1-mem/mem.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds  --build-id  -o /home/ubuntu/linux/tools/labs/skels/./kernel_api/1-mem/mem.ko /home/ubuntu/linux/tools/labs/skels/./kernel_api/1-mem/mem.o /home/ubuntu/linux/tools/labs/skels/./kernel_api/1-mem/mem.mod.o ;  true
